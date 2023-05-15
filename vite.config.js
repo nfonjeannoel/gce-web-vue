e00807fb945dd2c27589e35f41cd421800b4933a
JEANNOEL <nfonjeannoel1@gmail.com>
2023-05-15 11:32:25 +0100

    add headers

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,9 @@ export default defineConfig({
         proxy: {
             '/api': {
                 target: 'https://jean.tdev-hub.space/fetch-result.php',
-                changeOrigin: true
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, ""),
             }
         }
     },
